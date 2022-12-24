f3afd473388689aa0de7b26a8706c8d55f9aa133
Cheng-K <adamkei2612@gmail.com>
2022-12-24 14:56:33 +0800

    Minor update on code formatting

--- app/screens/AppSplashScreen.js
+++ app/screens/AppSplashScreen.js
@@ -1,6 +1,7 @@
 import LottieView from "lottie-react-native";
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+
 import Screen from "../components/Screen";
 import colors from "../config/colors";
 
--- app/screens/LoadingScreen.js
+++ app/screens/LoadingScreen.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import LottieView from "lottie-react-native";
+
 import Screen from "../components/Screen";
 import colors from "../config/colors";
 
